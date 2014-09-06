531983e44b973f553921ca66ff92e2169add9ea0
Michael Gilliland <mjg.py3@gmail.com>
2014-09-05 20:50:34 -0400

    Made token types functs...
    Why?
      1. No magic value (e.g. 0, or 1)
      2. `TokenType.integer` is easier to type and seems to make more sense
         (to me at least) than calls

--- src/lexer.py
+++ src/lexer.py
@@ -2,14 +2,11 @@ class Lexer:
     def tokenize(self, characters):
         token = lambda: None
         if characters[0] in ["'", '"']:
-          token.is_a = lambda ttype: ttype == TokenType().string()
+          token.is_a = lambda ttype: ttype == TokenType.string
         else:
-          token.is_a = lambda ttype: ttype == TokenType().integer()
+          token.is_a = lambda ttype: ttype == TokenType.integer
         return token
 
 class TokenType:
-    def integer(self):
-        return 0
-
-    def string(self):
-        return 1
+    def integer(): pass
+    def string(): pass
--- src/lexer_specs.py
+++ src/lexer_specs.py
@@ -6,15 +6,15 @@ from lexer import Lexer, TokenType
 class LexerSpecs(unittest.TestCase):
     def test_when_lexing_integer_type_is_integer(self):
         token = Lexer().tokenize('2814')
-        self.assertTrue(token.is_a(TokenType().integer()))
+        self.assertTrue(token.is_a(TokenType.integer))
 
     def test_when_lexing_a_single_quoted_thing_its_type_is_string(self):
         token = Lexer().tokenize("'single quoted!!!'")
-        self.assertTrue(token.is_a(TokenType().string()))
+        self.assertTrue(token.is_a(TokenType.string))
 
     def test_when_lexing_a_double_quoted_thing_its_type_is_string(self):
         token = Lexer().tokenize('"double quoted = more work"')
-        self.assertTrue(token.is_a(TokenType().string()))
+        self.assertTrue(token.is_a(TokenType.string))
 
 if __name__ == '__main__':
     unittest.main()
