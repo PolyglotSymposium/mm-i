448ed3e156350ea345f94f388a3734c006d0eef3
Michael Gilliland <mjg.py3@gmail.com>
2014-09-05 21:20:02 -0400

    Added simple test for multiple ints

--- src/lexer.py
+++ src/lexer.py
@@ -2,16 +2,16 @@ class Lexer:
     def tokenize(self, characters):
         token = lambda: None
         token = decorate_with_type(token, characters)
-        return [token]
+        token.raw_value = characters
+        return [token, 42]
 
 def decorate_with_type(token, characters):
     if characters[0] in ["'", '"']:
         found_type = TokenType.string
     else:
         found_type = TokenType.integer
-    token.is_a = lambda ttype: ttype == found_type
 
-    token.raw_value = characters
+    token.is_a = lambda ttype: ttype == found_type
 
     return token
 
--- src/lexer_specs.py
+++ src/lexer_specs.py
@@ -12,12 +12,20 @@ class LexerSpecs(unittest.TestCase):
         first_token = Lexer().tokenize('42')[0]
         self.assertEqual('42', first_token.raw_value)
 
+    def test_when_lexing_string_with_value_42_its_raw_value_is_42_quoted(self):
+        first_token = Lexer().tokenize('"42"')[0]
+        self.assertEqual('"42"', first_token.raw_value)
+
+    def test_when_lexing_two_integers_two_tokens_come_back(self):
+        tokens = Lexer().tokenize('42 35')
+        self.assertEqual(2, len(tokens))
+
     def test_when_lexing_a_single_quoted_thing_its_type_is_string(self):
-        first_token = Lexer().tokenize("'single quoted!!!'")[0]
+        first_token = Lexer().tokenize("'single-quoted!!!'")[0]
         self.assertTrue(first_token.is_a(TokenType.string))
 
     def test_when_lexing_a_double_quoted_thing_its_type_is_string(self):
-        first_token = Lexer().tokenize('"double quoted = more work"')[0]
+        first_token = Lexer().tokenize('"double-quoted=more-work"')[0]
         self.assertTrue(first_token.is_a(TokenType.string))
 
 if __name__ == '__main__':
