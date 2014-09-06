1aa23c62696ea3e6cc056464ee59269d9ea10da5
Michael Gilliland <mjg.py3@gmail.com>
2014-09-05 21:04:11 -0400

    Separated out type logic

--- src/lexer.py
+++ src/lexer.py
@@ -1,11 +1,17 @@
 class Lexer:
     def tokenize(self, characters):
         token = lambda: None
-        if characters[0] in ["'", '"']:
-          token.is_a = lambda ttype: ttype == TokenType.string
-        else:
-          token.is_a = lambda ttype: ttype == TokenType.integer
+        token = decorate_with_type(token, characters)
         return [token]
+
+def decorate_with_type(token, characters):
+    if characters[0] in ["'", '"']:
+        token.is_a = lambda ttype: ttype == TokenType.string
+    else:
+        token.is_a = lambda ttype: ttype == TokenType.integer
+
+    return token
+
 
 class TokenType:
     def integer(): pass
