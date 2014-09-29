fc2c4dee0946fe2d263328f3c715531003e2806a
Kazark <kazark@zoho.com>
2014-09-29 15:35:09 -0400

    Continued to work on initial sketch of expressions

--- src/mmi_parser.py
+++ src/mmi_parser.py
@@ -3,6 +3,9 @@ import mmi_token as token
 def constant_value():
     pass
 
+def function_call():
+    pass
+
 class Expression:
     def is_a(self, exprtype):
         return True
--- src/mmi_parser_specs.py
+++ src/mmi_parser_specs.py
@@ -2,19 +2,27 @@
 
 import unittest
 import mmi_token as token
-from mmi_parser import Parser, constant_value
+from mmi_parser import Parser, constant_value, function_call
+
+def one_integer():
+    yield token.integer()
+
+def two_identifiers():
+    yield token.identifier()
+    yield token.identifier()
 
 class ParserSpecs(unittest.TestCase):
     def test_single_token_in_input_stream_results_in_single_expression_in_output_stream(self):
-        def one_token():
-            yield token.integer()
-        expressions = Parser().parse(one_token())
+        expressions = Parser().parse(one_integer())
         self.assertEqual(1, len(list(expressions)))
-    def test_single_token_in_input_stream_results_in_constant_value_expression(self):
-        def one_token():
-            yield token.integer()
-        expr = next(Parser().parse(one_token()))
+
+    def test_single_integer_token_parses_to_constant_value_expression(self):
+        expr = next(Parser().parse(one_integer()))
         self.assertTrue(expr.is_a(constant_value()))
+
+    def test_two_identifier_tokens_parse_to_function_application(self):
+        expr = next(Parser().parse(two_identifiers()))
+        self.assertTrue(expr.is_a(function_call()))
 
 if __name__ == '__main__':
     unittest.main()
