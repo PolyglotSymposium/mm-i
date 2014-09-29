bd9b43c7ef48c4fc87b9b5de4e93a0f3282cf2a1
Kazark <kazark@zoho.com>
2014-09-29 15:22:47 -0400

    Bootstrapped a parser

---
+++ src/mmi_parser.py
@@ -0,0 +1,5 @@
+import mmi_token as token
+
+class Parser:
+    def parse(self, tokens):
+        yield True
---
+++ src/mmi_parser_specs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import unittest
+import mmi_token as token
+from mmi_parser import Parser
+
+class ParserSpecs(unittest.TestCase):
+    def test_single_token_in_input_stream_results_in_single_expression_in_output_stream(self):
+        def one_token():
+            yield token.integer()
+        expressions = Parser().parse(one_token())
+        self.assertEqual(1, len(list(expressions)))
+
+if __name__ == '__main__':
+    unittest.main()
