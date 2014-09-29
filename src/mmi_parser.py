6c12f34fd8f6853187951c6c0eed8956421552f1
Kazark <kazark@zoho.com>
2014-09-29 15:28:34 -0400

    Began to sketch out the shape of expressions

--- src/mmi_parser.py
+++ src/mmi_parser.py
@@ -1,5 +1,12 @@
 import mmi_token as token
 
+def constant_value():
+    pass
+
+class Expression:
+    def is_a(self, exprtype):
+        return True
+
 class Parser:
     def parse(self, tokens):
-        yield True
+        yield Expression()
--- src/mmi_parser_specs.py
+++ src/mmi_parser_specs.py
@@ -2,7 +2,7 @@
 
 import unittest
 import mmi_token as token
-from mmi_parser import Parser
+from mmi_parser import Parser, constant_value
 
 class ParserSpecs(unittest.TestCase):
     def test_single_token_in_input_stream_results_in_single_expression_in_output_stream(self):
@@ -10,6 +10,11 @@ class ParserSpecs(unittest.TestCase):
             yield token.integer()
         expressions = Parser().parse(one_token())
         self.assertEqual(1, len(list(expressions)))
+    def test_single_token_in_input_stream_results_in_constant_value_expression(self):
+        def one_token():
+            yield token.integer()
+        expr = next(Parser().parse(one_token()))
+        self.assertTrue(expr.is_a(constant_value()))
 
 if __name__ == '__main__':
     unittest.main()
