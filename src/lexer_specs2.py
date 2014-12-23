ed113ee5b976cc5385ce585c7a6f557272782802
Kazark <kazark@zoho.com>
2014-12-22 19:08:37 -0500

    Began to actually use Freeverse in its early form

--- src/lexer_specs2.py
+++ src/lexer_specs2.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 
-from freeform import spec, should
+from freeverse import SpecFor, Should, Expect, FlatOutput
 from lexer import *
 
-spec('The lexer', Lexer,
+spec = SpecFor('The MM/I Lexer')
+
+spec.add('The lexer', Lexer,
     ('should parse', lambda lexer: lexer.tokenize,
         ('a ( character', lambda: '(',
-            should('as a left-parenthesis token', lambda token:
-                token.should_equal(token.left_paren())
+            ('as a left-parenthesis token', lambda token:
+                token.should_be(token.left_paren())
             )
         ),
         ('a ) character', lambda: ')',
-            should('as a right-parenthesis token', lambda token:
-                token.should_equal(token.right_paren())
+            ('as a right-parenthesis token', lambda token:
+                token.should_be(token.right_paren())
             )
         )
     )
 )
 
 if __name__ == '__main__':
-    freeform.run_specs()
+    import sys
+    spec.run_and_write_to(FlatOutput(sys.stdout))
--- src/mmi_parser_specs2.py
+++ src/mmi_parser_specs2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from freeverse import spec, expect
+from freeverse import SpecFor, Should, Expect, FlatOutput
 
 import mmi_token as token
 from mmi_parser import Parser
@@ -9,22 +9,23 @@ from mmi_ast import ConstantValue, FunctionCall
 def one_integer(x=0):
     yield token.integer(x)
 
-spec(('The MM/I Parser', Parser,
-    ('when it parses', lambda parser: parser.parse,
-        ('a single-token input stream', lambda parse: parse(one_integer()),
-            ('should yield a single-expression output stream', lambda stream:
-                expect(len(list(stream))).to_be(1)
-            )
-        ),
-        ('a stream which begins with an integer token', lambda parse: parse(one_integer(7)),
-            ('the first expression in the output stream', lambda ouput: next(ouput),
-                ('should be a single, constant-value expression', lambda expr:
-                    expect(expr.is_a(ConstantValue)).to_be(True)
-                )
+spec = SpecFor('The MM/I Parser')
+
+spec.add('Parsing', lambda: Parser().parse,
+    ('a single-token input stream', lambda parse: parse(one_integer()),
+        Should('yield a single-expression output stream', lambda stream:
+            Expect(len(list(stream))).to_be(1)
+        )
+    ),
+    ('a stream which begins with an integer token', lambda parse: parse(one_integer(7)),
+        ('the first expression in the output stream', lambda ouput: next(ouput),
+            Should('be a single, constant-value expression', lambda expr:
+                Expect(expr.is_a(ConstantValue)).to_be(True)
             )
         )
     )
-))
+)
 
 if __name__ == '__main__':
-    freeverse.run_specs()
+    import sys
+    spec.run_and_write_to(FlatOutput(sys.stdout))
